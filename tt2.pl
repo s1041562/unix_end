be1edd38d817ec9bccac89c23e6e1d6acda763a5
s1041562 <910484jim@gmail.com>
2019-06-09 17:09:42 +0800

    final

--- tt2.pl
+++ tt2.pl
@@ -14,14 +14,22 @@ binmode(STDIN, ':encoding(utf8)');
 binmode(STDOUT, ':encoding(utf8)');
 binmode(STDERR, ':encoding(utf8)');
 
+
+say "輸入想爬看板";
+my $input = <STDIN>;
+say "輸入想爬頁數";
+my $target_page = <STDIN>;
+chomp $input;
+
 #open(FILE,"<result_.txt")||die "$!";
-my $url = "https://www.ptt.cc/bbs/C_Chat/index.html";
+my $url = "https://www.ptt.cc/bbs/$input/index.html";
 my $count = 0;
 
 my $next = "";
 open(FILE_TITLE,">TITLE.txt")||die "$!";
 
-while($count <=10){
+while($count != $target_page){
+	say "$count";
 	open(FILE,">Result.txt")||die "$!";
 	my $scraper = scraper {
     process 'a', "urls[]" => '@href';
@@ -56,21 +64,23 @@ while($count <=10){
 	}
 	close(FILE);
 	$count = $count+1;
-	say "$count";
+	
 }
 close(FILE_TITLE);
 
-
+#configure Useragent
 my $ua = LWP::UserAgent->new;
 $ua->agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36");
+
 my $num = 0;
 open(FILE_,"<TITLE.txt")||die "$!";
+system("mkdir copy_art");
 
 my $number = 0;
 my $LINE;
 while (defined ($LINE = <FILE_>)){
+    my $filename_by_title;
     chomp $LINE;
-    open(FILE_TEXT,">copy_art/$num.txt")||die "$!";
     say "目前網址：","$LINE";
 
     my $resp = $ua->get($LINE);
@@ -78,9 +88,17 @@ while (defined ($LINE = <FILE_>)){
 
     my $tree = HTML::TreeBuilder->new; 
     $tree->parse($resp);
-
+    
+    if($resp =~ /<meta property="og:title" content="(.*)">/){
+        $filename_by_title = "$1";
+        say "$filename_by_title";
+    }
+  
+    open(FILE_TEXT,">copy_art/$filename_by_title.txt")||die "$!";
+    
     foreach my $title ($tree->look_down(class => 'article-metaline',)) {
         say FILE_TEXT $title->as_trimmed_text();
+        
     }
 
     my @values = split('\n', $resp);  
@@ -92,7 +110,6 @@ while (defined ($LINE = <FILE_>)){
             if($val =~ /<span/){
                 $print_content = 0;
             }else{
-                #say "$val";
                 say FILE_TEXT $val;
             }
         }
@@ -104,7 +121,6 @@ while (defined ($LINE = <FILE_>)){
     foreach my $anchor ($tree->look_down(class => 'push',)) {
 
         my $temp = $anchor->as_trimmed_text();
-        #say $temp;
         say FILE_TEXT $temp;
     }
     close(FILE_TEXT);
